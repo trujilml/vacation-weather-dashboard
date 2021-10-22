fd6d7c7ad66c28712b81bb0812dc207227a6db4d
trujilml <mickeytrj@gmail.com>
2021-10-21 20:39:57 -0500

    added weather blocks updated css design and implemented beginning weather function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,22 @@
+//below sets date and current time for user on header - used moment.js
 var todayEl = document.querySelector("#today"); 
 var todayTime = moment();
 
 todayEl.textContent = todayTime.format("dddd, MMMM Do YYYY, h:mm:ss a");
 
+const APIKey = "9ec33c9f56aaa842560b13552e741183";
+var city = document.querySelector("#city");
+
+
+function weather(city) {
+
+    let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+    axios.get(url).then (function(response) {
+        console.log(response);
+
+
+
+
+    })
+}
