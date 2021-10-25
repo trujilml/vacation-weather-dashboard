682015e5ed34068608587ae06d6941852a6cd620
trujilml <mickeytrj@gmail.com>
2021-10-25 11:45:28 -0500

    finished current function - may need to restructure as my elements are currently seen on null on console and search asset is not working - link cannot be retrieved - may have to rewrite some functions

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,10 +1,10 @@
 //below sets date and current time for user on header - used moment.js
-var todayEl = document.querySelector("#today"); 
-var todayTime = moment();
+// var todayEl = document.querySelector("#today"); 
+// var todayTime = moment();
 
-todayEl.textContent = todayTime.format("dddd, MMMM Do YYYY, h:mm:ss a");
+// todayEl.textContent = todayTime.format("dddd, MMMM Do YYYY, h:mm:ss a");
 
-function retrieve() {
+function retrieveThis() {
 
     var weatherToday = document.getElementById("#weathernow");
     var city = document.getElementById("#city");
@@ -15,18 +15,19 @@ function retrieve() {
     var windSpeed = document.getElementById("#wind-speed"); 
     var currentUvIndex = document.getElementById("#UV-index");
 
-    var searchIt = document.getElementById("#search");
-    let searchLog = JSON.parse(localStorage.getItem("search")) || [];
+    var searchIt = document.getElementById("#search-btn");
     var searchHistory = document.getElementById("#history");
+    var clearIt = document.getElementById("#clear-search");
 
     var fiveDayForecast = document.getElementById("#fivedayboard");
 
-//const apiKey = "";
+    let searchLog = JSON.parse(localStorage.getItem("search")) || [];
+
 
 //API key will be implemented when project is nearly done - in order to not get github guardian alerts on my phone multiple times per day
 //wait till next week to turn in - for now, let's work on
 
-function weather(city) {
+function weatherRetrieval(city) {
 
     let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
@@ -82,20 +83,74 @@ function weather(city) {
 
         const forecastLong = document.querySelectorAll(".forecast");
         for (var i = 0; i < forecastLong.length; i++) {
+            forecastLong[i].innerHTML = "";
+            var forecastIndex = i * 8 + 4;
+            var forecastDate = new Data (response.data.list[forecastIndex].dt * 1000);
+            var forecastDay = forecastDate.getDate();
+            var forecastMonth = forecastDate.getMonth() + 1;
+            var forecastYear = forecaseDate.getFullYear();
+            var currentForecastDate = document.createElement("p");
+            currentForecastDate.setAttribute("class","mt-3 mb-0 current-forecast-date");
+            currentForecastDate.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+            forecastLong[i].append(currentForecastDate);
 
+            //icons representing current weather
+            var forecastWeatherImage = document.createElement("img");
+            forecastWeatherImage.setAttribute("src", "https://openweathermap.org/img/wn/" + response.data.list[forecastIndex].weather[0].icon + "@2x.png");
+            forecastWeatherImage.setAttribute("alt", response.data.list[forecastIndex].weather[0].description);
+            forecastLong[i].append(forecastWeatherImage);
+
+            var forecastTemperature = document.createElement("p");
+            forecastTemperature.innerHTML = "Temperature: " + k2f(response.data.list[forecastIndex].main.temp) + "&#176";
+            forecastLong[i].append(forecastTemperature);
+
+            var forecastHumidity = document.createElement("p");
+            forecastHumidity.innerHTML = "Humidity: " + (response.data.list[forecastIndex].main.humidity) + "%";
+            forecastLong[i].append(forecastHumidity);
         }
+    })
 });
+}
+
+
+searchIt.addEventListener("click", function() {
+    const searchCity = city.value;
+    weatherRetrieval(searchCity);
+    searchLog.push(searchCity);
+    localStorage.setItem("search", JSON.stringify(searchLog));
+    retrieveSearchLog();
 })
 
- 
+clearIt.addEventListener("click", function() {
+    localStorage.clear();
+    searchLog = [];
+    retrieveSearchLog();
+})
+
 function k2f(K) {
     return Math.floor((K - 273.15) * 1.8 + 32);
 }
 
-function searchHistory () {
-
+function retrieveSearchLog() {
+ searchHistory.innerHTML = "";
+ for (var i = 0; i < searchHistory.length; i++) {
+     const pastSearchItem = document.createElement("input");
+     pastSearchItem.searchAttribute("type","text");
+     pastSearchItem.searchAttribute("readonly", true);
+     pastSearchItem.searchAttribute("class", "form-control d-block bg-white");
+     pastSearchItem.searchAttribute("value", searchLog[i]);
+     pastSearchItem.addEventListener("click", function(){
+         weatherRetrieval(pastSearchItem.value);
+     })
+     searchHistory.append(pastSearchItem);
+    }
 }
 
+retrieveSearchLog();
 
+if (searchLog.length > 0) {
+    weatherRetrieval(searchLog[searchLog.length - 1]);
 }
 }
+
+retrieveThis();
